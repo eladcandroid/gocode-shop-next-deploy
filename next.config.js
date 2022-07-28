2f9bb9ad674f6193613bca644c7c58e3d3ef36e6
Elad Cohen <eladc.android@gmail.com>
2022-07-28 10:45:02 +0300

    Revert next.config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const dev = process.env.NODE_ENV !== "production";
-
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://gocode-shop-next-deploy.vercel.app/";
-
 const nextConfig = {
   reactStrictMode: false,
-  server,
 };
 
 module.exports = nextConfig;
